98bb009e9b21370acef9e37263d4b47fdb2df547
BOTZEMPIRE <chaudharyji0805@gmail.com>
2025-12-31 11:59:45 +0530

    Update __init__.py

--- SagiriRobot/__init__.py
+++ SagiriRobot/__init__.py
@@ -5,14 +5,15 @@ import time
 
 import telegram.ext as tg
 from aiohttp import ClientSession
-from arq import arq
+from arq import Arq
 from redis import StrictRedis
-from pyrogram import Client, errors
+from pyrogram import Client
 from telethon import TelegramClient
 
 StartTime = time.time()
 
-# enable logging
+# ================= LOGGING ================= #
+
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
@@ -22,193 +23,133 @@ logging.basicConfig(
 logging.getLogger("apscheduler").setLevel(logging.ERROR)
 logging.getLogger("telethon").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
 LOGGER = logging.getLogger(__name__)
 
-# if version < 3.6, stop bot.
-if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    LOGGER.error(
-        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
-    )
-    quit(1)
+# ================= PYTHON VERSION ================= #
 
-ENV = bool(os.environ.get("ENV", False))
+if sys.version_info < (3, 8):
+    LOGGER.error("Python 3.8+ is required. Bot is quitting.")
+    sys.exit(1)
+
+# ================= ENV ================= #
+
+def strtobool(val):
+    return str(val).lower() in ("true", "1", "yes", "y")
+
+ENV = strtobool(os.environ.get("ENV", False))
 
 if ENV:
-    API_ID = int(os.environ.get("API_ID", None))
-    API_HASH = os.environ.get("API_HASH", None)
-    ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
-    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
-    CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    TOKEN = os.environ.get("TOKEN")
+
+    ALLOW_CHATS = strtobool(os.environ.get("ALLOW_CHATS", True))
+    ALLOW_EXCL = strtobool(os.environ.get("ALLOW_EXCL", False))
+    DEL_CMDS = strtobool(os.environ.get("DEL_CMDS", False))
+    INFOPIC = strtobool(os.environ.get("INFOPIC", True))
+    STRICT_GBAN = strtobool(os.environ.get("STRICT_GBAN", True))
+
+    CASH_API_KEY = os.environ.get("CASH_API_KEY")
     DB_URI = os.environ.get("DATABASE_URL")
-    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
-    INFOPIC = bool(os.environ.get("INFOPIC", "True"))
+    EVENT_LOGS = os.environ.get("EVENT_LOGS")
+
     LOAD = os.environ.get("LOAD", "").split()
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
-    REDIS_URL = os.environ.get("REDIS_URL", None)
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
+
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
+    REDIS_URL = os.environ.get("REDIS_URL")
+
     START_IMG = os.environ.get(
         "START_IMG", "https://telegra.ph/file/40eb1ed850cdea274693e.jpg"
     )
-    PM_START_IMG = os.environ.get("PM_START_IMG", None)
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
+    PM_START_IMG = os.environ.get("PM_START_IMG")
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Gojo_support_chat")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
-    TOKEN = os.environ.get("TOKEN", None)
-    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
+
+    TIME_API_KEY = os.environ.get("TIME_API_KEY")
     WORKERS = int(os.environ.get("WORKERS", 8))
-    arq_API_URL = os.environ.get("arq_API_URL", "http://thearq.tech?")
-    arq_API_KEY = os.environ.get("arq_API_KEY", "FZPYYN-EKAYFX-RNPLEJ-DRVAPH-arq")    
- 
-    try:
-        OWNER_ID = int(os.environ.get("OWNER_ID", None))
-    except ValueError:
-        raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
-    try:
-        BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
+    OWNER_ID = int(os.environ.get("OWNER_ID"))
 
-    try:
-        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
+    BL_CHATS = set(map(int, os.environ.get("BL_CHATS", "").split()))
+    DRAGONS = set(map(int, os.environ.get("DRAGONS", "").split()))
+    DEV_USERS = set(map(int, os.environ.get("DEV_USERS", "").split()))
+    DEMONS = set(map(int, os.environ.get("DEMONS", "").split()))
+    TIGERS = set(map(int, os.environ.get("TIGERS", "").split()))
+    WOLVES = set(map(int, os.environ.get("WOLVES", "").split()))
 
-    try:
-        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "").split())
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "").split())
-    except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
-
-    try:
-        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
+    # ARQ
+    ARQ_API_URL = os.environ.get("ARQ_API_URL", "https://arq.hamker.dev")
+    ARQ_API_KEY = os.environ.get("ARQ_API_KEY")
 
 else:
-    from DazaiRobot.config import Development as Config
+    raise RuntimeError("ENV must be enabled on Heroku")
 
-    API_ID = Config.API_ID
-    API_HASH = Config.API_HASH
-    ALLOW_CHATS = Config.ALLOW_CHATS
-    ALLOW_EXCL = Config.ALLOW_EXCL
-    CASH_API_KEY = Config.CASH_API_KEY
-    DB_URI = Config.SQLALCHEMY_DATABASE_URI
-    DEL_CMDS = Config.DEL_CMDS
-    EVENT_LOGS = Config.EVENT_LOGS
-    INFOPIC = Config.INFOPIC
-    LOAD = Config.LOAD
-    MONGO_DB_URI = Config.MONGO_DB_URI
-    REDIS_URL = Config.REDIS_URL
-    NO_LOAD = Config.NO_LOAD
-    START_IMG = Config.START_IMG
-    PM_START_IMG = Config.PM_START_IMG
-    STRICT_GBAN = Config.STRICT_GBAN
-    SUPPORT_CHAT = Config.SUPPORT_CHAT
-    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
-    TOKEN = Config.TOKEN
-    TIME_API_KEY = Config.TIME_API_KEY
-    WORKERS = Config.WORKERS
-    arq_API_KEY = Config.arq_API_KEY
-    arq_API_URL = Config.arq_API_URL 
+# ================= USERS ================= #
 
-    try:
-        OWNER_ID = int(Config.OWNER_ID)
-    except ValueError:
-        raise Exception("Your OWNER_ID variable is not a valid integer.")
+DRAGONS.update(
+    {
+        OWNER_ID,
+        6432025901,
+        6058139652,
+        6864672519,
+        6944225218,
+        6438969887,
+    }
+)
 
-    try:
-        BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
+DEV_USERS.update({OWNER_ID, 6171176459, 6058139652})
 
-    try:
-        DRAGONS = set(int(x) for x in Config.DRAGONS or [])
-        DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
-
-    try:
-        DEMONS = set(int(x) for x in Config.DEMONS or [])
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        TIGERS = set(int(x) for x in Config.TIGERS or [])
-    except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
-
-    try:
-        WOLVES = set(int(x) for x in Config.WOLVES or [])
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-
-DRAGONS.add(6432025901)
-DRAGONS.add(6058139652)
-DRAGONS.add(6864672519)
-DRAGONS.add(6944225218)
-DRAGONS.add(6438969887)
-DRAGONS.add(OWNER_ID)
-DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(6171176459)
-DEV_USERS.add(6058139652)
+# ================= REDIS ================= #
 
 REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
 try:
-
     REDIS.ping()
+    LOGGER.info("[Sagiri]: Redis connected successfully")
+except Exception as e:
+    raise RuntimeError("Redis is not reachable") from e
 
-    LOGGER.info("[Sagiri]: Connecting to redis")
-except BaseException:
-
-    raise Exception("[Sagiri ERROR]: Redis Database Is Not Alive, Please Check Again.")
-
-finally:
-
-   REDIS.ping()
-
-   LOGGER.info("[Sagiri]: Connection to Redis Database Established Successfully!")
-
-
+# ================= CLIENTS ================= #
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("Sagiri", API_ID, API_HASH)
-
-pbot = Client("SagiriRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
+
+telethn = TelegramClient("Sagiri", API_ID, API_HASH)
+pbot = Client("SagiriRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+
 aiohttpsession = ClientSession()
 
-print("[INFO]: Getting Bot Info...")
 BOT_ID = dispatcher.bot.id
 BOT_NAME = dispatcher.bot.first_name
 BOT_USERNAME = dispatcher.bot.username
 
-# arq Client
-print("[INFO]: INITIALIZING arq CLIENT...")
-arq = arq(arq_API_URL, arq_API_KEY, aiohttpsession)
+# ================= ARQ CLIENT ================= #
 
-DRAGONS = list(DRAGONS) + list(DEV_USERS)
+if not ARQ_API_KEY:
+    LOGGER.warning("ARQ API key not set, ARQ features disabled")
+    arq = None
+else:
+    arq = Arq(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
+    LOGGER.info("[Sagiri]: ARQ client initialized")
+
+# ================= LIST FINALIZE ================= #
+
+DRAGONS = list(DRAGONS | DEV_USERS)
 DEV_USERS = list(DEV_USERS)
 WOLVES = list(WOLVES)
 DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
-# Load at end to ensure all prev variables have been set
+# ================= HANDLERS ================= #
+
 from SagiriRobot.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
 )
 
-# make sure the regex handler can take extra kwargs
 tg.RegexHandler = CustomRegexHandler
 tg.CommandHandler = CustomCommandHandler
 tg.MessageHandler = CustomMessageHandler
